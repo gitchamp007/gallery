baca99715b3fe665ccbbac0042e2c7ec1b03f8de
Raj Singh <121677665+gitchamp007@users.noreply.github.com>
2023-10-14 16:14:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const img_box = document.getElementById("click_img_box");
 const clicked_img = document.getElementById("clicked_img");
 const download_link = document.getElementById("download");
@@ -6,78 +5,77 @@ const gallery = document.getElementById("img_gal");
 
 var count = 0;
 
-var img = ['Images/img1.jpg', 'Images/1.jpg', 'Images/2.jpg', 'http://127.0.0.1:5500/Images/img2.jpg', 'http://127.0.0.1:5500/Images/img3.jpg', 'http://127.0.0.1:5500/Images/3.jpg', 'http://127.0.0.1:5500/Images/img4.jpg', 'http://127.0.0.1:5500/Images/4.jpg', 'http://127.0.0.1:5500/Images/5.jpg', 'http://127.0.0.1:5500/Images/img5.jpg', 'http://127.0.0.1:5500/Images/6.jpg', 'http://127.0.0.1:5500/Images/img6.jpg', 'http://127.0.0.1:5500/Images/img7.jpg', 'http://127.0.0.1:5500/Images/7.jpg', 'http://127.0.0.1:5500/Images/img8.jpg', 'http://127.0.0.1:5500/Images/8.jpg'];
+var img = [
+    'images/img1.jpg',
+    'images/1.jpg',
+    'images/2.jpg',
+    'images/img2.jpg',
+    'images/img3.jpg',
+    'images/3.jpg',
+    'images/img4.jpg',
+    'images/4.jpg',
+    'images/5.jpg',
+    'images/img5.jpg',
+    'images/6.jpg',
+    'images/img6.jpg',
+    'images/img7.jpg',
+    'images/7.jpg',
+    'images/img8.jpg',
+    'images/8.jpg'
+  ];
 
 
-
-for (let i = 0; i < img.length; i++)
-{
-    const image = document.createElement("img");
-    image.src = img[i];
-    image.setAttribute("onclick", "openimg(this.src)")
-    gallery.appendChild(image);
+for (let i = 0; i < img.length; i++) {
+  const image = document.createElement("img");
+  image.src = img[i];
+  image.setAttribute("data-index", i);
+  image.addEventListener("click", function() {
+    openimg(this);
+  });
+  gallery.appendChild(image);
 }
 
-
-function openimg(pic)
-{
-    img_box.style.display = "flex";
-    clicked_img.src = pic;
-    count = img.indexOf(pic);
+function openimg(image) {
+  img_box.style.display = "flex";
+  clicked_img.src = image.src;
+  count = parseInt(image.getAttribute("data-index"));
+  updateDownloadLink();
 }
 
-
-function closeimg()
-{
-    img_box.style.display = "none";
+function closeimg() {
+  img_box.style.display = "none";
+  clicked_img.src = "";
 }
 
-function prev()
-{ 
-    if(count <= 0) 
-    {
-        count = img.length;
-    }
-    count--;
-    return setImg()
+function prev() {
+  count = (count - 1 + img.length) % img.length;
+  setImg();
+  updateDownloadLink();
 }
 
-function next()
-{
-    if (count >= img.length - 1)
-    {
-        count = -1;
-    }
-    count++;
-    return setImg()
+function next() {
+  count = (count + 1) % img.length;
+  setImg();
+  updateDownloadLink();
 }
 
-function download()
-{
-    download_link.href = img[count];
+function updateDownloadLink() {
+  download_link.href = img[count];
 }
 
-
-function setImg()
-{
-    console.log(count);
-    return clicked_img.setAttribute('src',img[count])
+function setImg() {
+  console.log(count);
+  clicked_img.src = img[count];
 }
-
 
 document.addEventListener('keydown', (event) => {
-    var name = event.key;
-    
-    if (name == 'ArrowLeft')
-    {
-        prev();
-    }
-    else if (name == 'ArrowRight')
-    {
-        next();
-    }
-    else if( name == 'Escape')
-    {
-        closeimg();
-    }
-  }, false);
+  var name = event.key;
+
+  if (name == 'ArrowLeft') {
+    prev();
+  } else if (name == 'ArrowRight') {
+    next();
+  } else if (name == 'Escape') {
+    closeimg();
+  }
+}, false);
